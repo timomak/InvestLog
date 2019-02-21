dd57b60ea89d512db91ba25dcc3b0ca14e939fa8
timo <tmakhlay2@gmail.com>
2019-02-20 18:49:53 -0800

    Started complete redesign.

--- InvestLog/InvestLog/AppDelegate.swift
+++ InvestLog/InvestLog/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         
         
         // Override point for customization after application launch.
-        let initialViewController  = Login()
+        let initialViewController  = LoginViewController()
         window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = initialViewController
         self.window?.makeKeyAndVisible()
